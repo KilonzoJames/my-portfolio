dce6a7fefdc6a22997ce6e7f20ea36c0965b31d3
KilonzoJames <jay.kilonzo@gmail.com>
2023-11-21 13:48:23 +0300

    Added all techstack images

--- src/components/techstack.tsx
+++ src/components/techstack.tsx
@@ -1,35 +1,87 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faJs, faHtml5, faCss3, faReact, faBootstrap, faPython, faGit } from '@fortawesome/free-brands-svg-icons';
-
+import React from 'react';
 
 function Techstack() {
   const techstack = [
-    { name: "REACT", className: "react", icon: faReact },
-    { name: "JAVASCRIPT", className: "javascript", icon: faJs},
-    { name: "TYPESCRIPT", className: "typescript" },
-    { name: "CSS", className: "css", icon: faCss3 },
-    { name: "TAILWIND CSS", className: "tailwind-css" },
-    { name: "BOOTSTRAP", className: "bootstrap", icon: faBootstrap },
-    { name: "HTML", className: "html", icon: faHtml5 },
-    { name: "FLASK", className: "flask" },
-    { name: "PYTHON", className: "python", icon:faPython },
-    { name: "PostgreSQL", className: "postgresql" },
-    { name: "SQLite", className: "sqlite" },
-    { name: "SQL", className: "sql" },
-    { name: "POSTMAN", className: "postman" },
-    { name: "GIT", className: "git", icon: faGit },
+    { 
+      name: "REACT", 
+      className: "react", 
+      icon:  <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="70" height="70"/>
+    },
+    { 
+      name: "JAVASCRIPT", 
+      className: "javascript", 
+      icon: <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width="70" height="70"/>
+    },
+    { 
+      name: "TYPESCRIPT", 
+      className: "typescript",
+      icon: <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" width="70" height="70"/>
+    },
+    { 
+      name: "CSS", 
+      className: "css", 
+      icon: <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="70" height="70"/>
+    },
+    {
+      name: "TAILWIND CSS",
+      className: "tailwind-css",
+      icon: <img src="https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" alt="tailwind" width="70" height="70" />,
+    },
+    { 
+      name: "BOOTSTRAP", 
+      className: "bootstrap", 
+      icon: <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="70" height="70"/>
+    },
+    { 
+      name: "HTML", 
+      className: "html", 
+      icon: <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="70" height="70"/>
+    },
+    { 
+      name: "FLASK", 
+      className: "flask", 
+      icon: <img src="https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-icon.svg" alt="flask" width="70" height="70"/>
+    },
+    { 
+      name: "PYTHON", 
+      className: "python", 
+      icon: <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="70" height="70"/>
+    },
+    { 
+      name: "PostgreSQL", 
+      className: "postgresql",
+      icon: <img src="https://www.vectorlogo.zone/logos/postgresql/postgresql-icon.svg" alt="postgresql" width="70" height="70"/>
+    },
+    { 
+      name: "SQLite", 
+      className: "sqlite",
+      icon: <img src="https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg" alt="sqlite" width="70" height="70"/>
+    },
+    { 
+      name: "SQL", 
+      className: "sql",
+      icon: <img src="https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg" alt="postman" width="70" height="70"/>
+    },
+    { 
+      name: "POSTMAN", 
+      className: "postman", 
+      icon: <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="git" width="70" height="70"/>
+    },
+    { 
+      name: "GIT", 
+      className: "git", 
+      icon: <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="70" height="70"/>
+    },
   ];
+  
+  
   
     const techList = techstack.map((tech, index) => (
         <li 
         key={index} 
         className={`skill border border-zinc-950 cursor-pointer hover:bg-green-400 py-4 w-[12rem] transition-all duration-400 ${tech.className}`}
         >
-         <span className="mr-2">
-          {tech.icon && <FontAwesomeIcon className="text-6xl" icon={tech.icon} />}
-          </span>
-
+          {tech.icon && <div>{tech.icon} </div>}
       {tech.name}
         </li>
       ));
@@ -37,15 +89,14 @@ function Techstack() {
 
       
   return (
-<div className="relative p-[2vw]">
-    <div className="flex flex-col justify-center">
-        <h2 className="text-3xl my-[2em] text-green-500 font-bold">TECH STACK</h2>
-        <div className='p-4'>
-          {techListContainer}
+    <div className="relative p-[2vw]">
+        <div className="flex flex-col justify-center">
+            <h2 className="text-3xl my-[2em] text-green-500 font-bold">TECH STACK</h2>
+            <div className='p-4'>
+              {techListContainer}
+            </div>
         </div>
-    </div>
-</div>
-  
+    </div>  
   )
 }
 
